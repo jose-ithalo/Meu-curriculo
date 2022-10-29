730452b7c4cd7c3a4535106bfac16f505380593c
José Ithalo <jose_ithalo@hotmail.com>
2022-10-28 21:55:10 -0300

    Melhorando a responsividade

--- src/mobile.js
+++ src/mobile.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector('.menu-icon');
 const closeButton = document.querySelector('.menu-close');
+const optionScreen = document.querySelector('.menu-screen');
 
 menu.addEventListener('click', function () {
     menu.style.display = 'none';
     closeButton.style.display = 'block';
+    optionScreen.style.display = 'flex';
 });
 
 closeButton.addEventListener('click', function () {
     menu.style.display = 'block';
     closeButton.style.display = 'none';
+    optionScreen.style.display = 'none';
 });
