c973b1297be9b9ec56120b0110d68badb156eb5e
José Ithalo <jose_ithalo@hotmail.com>
2023-01-09 12:38:30 -0300

    Função de abrir e fechar terceiro modal adicionada

--- src/pages/projects/script.js
+++ src/pages/projects/script.js
@@ -1,7 +1,9 @@
 const firstCard = document.querySelector('#first');
 const secondCard = document.querySelector('#second');
+const tirdCard = document.querySelector('#tird');
 const firstModal = document.querySelector('#first-modal');
 const secondModal = document.querySelector('#second-modal');
+const tirdModal = document.querySelector('#tird-modal');
 const btnClose = document.querySelectorAll('.btn-close');
 
 firstCard.addEventListener('click', function () {
@@ -12,9 +14,14 @@ secondCard.addEventListener('click', function () {
     secondModal.style.display = 'flex';
 })
 
+tirdCard.addEventListener('click', function () {
+    tirdModal.style.display = 'flex';
+})
+
 btnClose.forEach(function (close) {
     close.addEventListener('click', function () {
         firstModal.style.display = 'none';
         secondModal.style.display = 'none';
+        tirdModal.style.display = 'none';
     });
 })
