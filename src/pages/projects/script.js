f953c9ec04642b211ab5051e8cf2194b33afe254
José Ithalo <jose_ithalo@hotmail.com>
2023-01-14 10:39:02 -0300

    Preparação do quarto card

--- src/pages/projects/script.js
+++ src/pages/projects/script.js
@@ -1,9 +1,11 @@
 const firstCard = document.querySelector('#first');
 const secondCard = document.querySelector('#second');
 const tirdCard = document.querySelector('#tird');
+const fourthCard = document.querySelector('#fourth');
 const firstModal = document.querySelector('#first-modal');
 const secondModal = document.querySelector('#second-modal');
 const tirdModal = document.querySelector('#tird-modal');
+const fourthModal = document.querySelector('#fourth-modal');
 const btnClose = document.querySelectorAll('.btn-close');
 
 firstCard.addEventListener('click', function () {
@@ -18,10 +20,15 @@ tirdCard.addEventListener('click', function () {
     tirdModal.style.display = 'flex';
 })
 
+fourthCard.addEventListener('click', function () {
+    fourthModal.style.display = 'flex';
+})
+
 btnClose.forEach(function (close) {
     close.addEventListener('click', function () {
         firstModal.style.display = 'none';
         secondModal.style.display = 'none';
         tirdModal.style.display = 'none';
+        fourthModal.style.display = 'none';
     });
 })
