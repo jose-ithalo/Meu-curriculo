4870de98ab8a5256c919edc30411013e7a3e70e5
José Ithalo <jose_ithalo@hotmail.com>
2023-12-06 10:48:26 -0300

    Novas referências criadas para manipulação do card

--- src/pages/projects/script.js
+++ src/pages/projects/script.js
@@ -2,10 +2,12 @@ const firstCard = document.querySelector('#first');
 const secondCard = document.querySelector('#second');
 const tirdCard = document.querySelector('#tird');
 const fourthCard = document.querySelector('#fourth');
+const fifthCard = document.querySelector('#fifth');
 const firstModal = document.querySelector('#first-modal');
 const secondModal = document.querySelector('#second-modal');
 const tirdModal = document.querySelector('#tird-modal');
 const fourthModal = document.querySelector('#fourth-modal');
+const fifthModal = document.querySelector('#fifth-modal');
 const btnClose = document.querySelectorAll('.btn-close');
 
 firstCard.addEventListener('click', function () {
@@ -24,11 +26,16 @@ fourthCard.addEventListener('click', function () {
     fourthModal.style.display = 'flex';
 })
 
+fifthCard.addEventListener('click', function () {
+    fifthModal.style.display = 'flex';
+})
+
 btnClose.forEach(function (close) {
     close.addEventListener('click', function () {
         firstModal.style.display = 'none';
         secondModal.style.display = 'none';
         tirdModal.style.display = 'none';
         fourthModal.style.display = 'none';
+        fifthModal.style.display = 'none';
     });
 })
